f95b89f0032038ca4a3f34d770bcaae20f6c6fdf
Dina Ayoub <dinaayoub@hotmail.com>
2020-12-11 21:29:19 -0800

    finished challenge and tests

--- javascript/code-challenges/linked-list/__tests__/linked-list.test.js
+++ javascript/code-challenges/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,48 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../linked-list');
 
 describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+  var linkedList = new LinkedList();
+  /*
+  Write tests to prove the following functionality:
+    [X] Can successfully instantiate an empty linked list
+    [X] Can properly insert into the linked list
+    [X] The head property will properly point to the first node in the linked list
+    [X] Can properly insert multiple nodes into the linked list
+    [X] Will return true when finding a value within the linked list that exists
+    [X] Will return false when searching for a value in the linked list that does not exist
+    [ ] Can properly return a collection of all the values that exist in the linked list
+ */
+
+  it('can successfully instantiate an empty linked list', () => {
+    expect(linkedList.head).toBe(null);
   });
+
+  it('can properly insert into the linked list', () => {
+    expect(linkedList.insert('foo').value).toStrictEqual('foo');
+  });
+
+  it('can properly have the head property point to the first node in the linked list' , () => {
+    expect(linkedList.head.value).toStrictEqual('foo');
+  });
+  it('can properly insert multiple nodes into the linked list', () => {
+    linkedList.insert('bar');
+    expect(linkedList.head.value).toStrictEqual('bar');
+    linkedList.insert('blah');
+    expect(linkedList.head.value).toStrictEqual('blah');
+  })
+  
+  it('can find a value and return true if it\'s in the linked list', () => {
+    expect(linkedList.includes('foo')).toBe(true);
+  })
+
+  it('can return false when the value is not in the linked list', () => {
+    expect(linkedList.includes('foobar')).toBe(false);
+  })
+
+  it('can return a collection of all the values that exist in the linked list', () => {
+    expect(linkedList.toString()).toStrictEqual('blah -> bar -> foo -> NULL');
+  })
 });
